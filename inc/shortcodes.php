75e6c10c107035c1f766b5d961edcad117d8e9d3
Remco Tolsma <info@remcotolsma.nl>
2013-04-12 11:36:07 +0200

    Fixed spacing.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -24,9 +24,7 @@ function issuu_pdf_embeder( $atts, $content = null ) {
 		'showFlipBtn'     => ( isset( $ips_options['show_flip_buttons'] ) && $ips_options['show_flip_buttons'] == 1 ) ? 'true' : 'false', 
 		'allowfullscreen' => ( isset( $ips_options['allow_full_screen'] ) && $ips_options['allow_full_screen'] == 1 ) ? 'true' : 'false',
 		'customLayout'    => ( isset( $ips_options['custom_layout'] ) ) ? $ips_options['custom_layout'] : false 
-		),
-		$atts
-	) ); 
+	), $atts ) ); 
 	
 	// Check if the required param is set
 	if ( empty( $issuu_pdf_id ) ) {
